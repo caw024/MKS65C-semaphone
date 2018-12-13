be3e77c4564e00df300336493eb3e5b47011c03c
Imad <ibelkebir@stuy.edu>
2018-12-13 14:34:09 -0500

    sem

--- control.c
+++ control.c
@@ -26,6 +26,11 @@ int main(int argc, char * argv[]) {
     printf("error %d: %s\n", errno, strerror(errno));
     exit(0);
   }
+  int val = semctl(semid, 0, GETVAL, 0);
+  if( val == 1 ){
+    printf("The game is currently being played by another user. Please wait your turn\n");
+    while( val1 = semctl(semid, 0, GETVAL, 0) );
+  }
   union semun us;
   us.val = 1;
   semctl(semid, 0, SETVAL, us);
