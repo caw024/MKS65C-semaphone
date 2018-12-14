eea1be939ae99463d4e58bee1fa5f150a2b3230b
Calvin Aw <caw@stuy.edu>
2018-12-13 23:30:33 -0500

    couldn't get semop to work

--- control.c
+++ control.c
@@ -13,75 +13,98 @@
 
 #define KEY 0xBEEFDEAD
 
- union semun {
-   int              val;    /* Value for SETVAL */
-   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-   unsigned short  *array;  /* Array for GETALL, SETALL */
-   struct seminfo  *__buf;  /* Buffer for IPC_INFO */
- };
+union semun {
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO */
+};
 
 int main(int argc, char * argv[]) {
-  int semid = semget(KEY, 1, 0644);
-  if (semid == -1) {
+
+  int semid = semget(KEY,1,0644);
+  if (semid == -1){
     printf("error %d: %s\n", errno, strerror(errno));
     exit(0);
   }
+  int val = semctl(semid, 0, GETVAL, 0); 
 
-  struct sembuf sb;
-  //if sem != 0, wait, else s--
-  sb.sem_up = -1;
-  sb.sem_num = 0;
-  sb.sem_flag = SEM_UNDO;
+  if( val == 1 ){ 
   
-  semop(semid,&sb,1);
+    printf("The game is currently being played by another user. Please wait your turn\n"); 
 
-  int val = semctl(semid, 0, GETVAL, 0);
-  if( val == 1 ){
-    printf("The game is currently being played by another user. Please wait your turn\n");
-    while( val = semctl(semid, 0, GETVAL, 0) );
+    while( val = semctl(semid, 0, GETVAL, 0) ); 
   }
-  union semun us;
-  us.val = 1;
-  semctl(semid, 0, SETVAL, us);
+   
+  union semun us; 
+  us.val = 1; 
+  semctl(semid, 0, SETVAL, us); 
 
-  int shmid = shmget(123456, 4, 0666);
-  int* data = shmat(shmid, 0, 0);
-  if( *data == -1 ){
-    printf("Error, %s\n", strerror(errno));
-    exit(0);
-  }
+  int shmid = shmget(123456, 4, 0666); 
+  int* data = shmat(shmid, 0, 0); 
+  if( *data == -1 ){ 
+    printf("Error, %s\n", strerror(errno)); 
+    exit(0); 
+  } 
 
-  FILE *fp = fopen("file.txt", "r");
-  if(!fp){
-    printf("Error, %s\n", strerror(errno));
-    exit(0);
-  }
-  printf("Last line in story: ");
-  fseek(fp, *data * -1, SEEK_END);
-  int c;
-  while(1){
-    c = fgetc(fp);
-    if( feof(fp) || c == 10 ){
-      break;
-    }
-    printf("%c", c);
-  }
-  fclose(fp);
+  FILE *fp = fopen("file.txt", "r"); 
+  if(!fp){ 
+    printf("Error, %s\n", strerror(errno)); 
+    exit(0); 
+  } 
+  printf("Last line in story: "); 
+  fseek(fp, *data * -1, SEEK_END); 
+  
+  int c; 
+  while(1){ 
+    c = fgetc(fp); 
+    if( feof(fp) || c == 10 ){ 
+      break; 
+    } 
+    printf("%c", c); 
+  } 
+  fclose(fp); 
 
+
+  
+  printf("\nEnter the next line for the story\n"); 
+  char* sentence = calloc(*data, sizeof(char)); 
+     
  
-  printf("\nEnter the next line for the story\n");
-  char* sentence = calloc(*data, sizeof(char));
-  scanf("%[^\n]", sentence);
-  strcat(sentence, "\n");
+  scanf("%[^\n]", sentence); 
+  
+  strcat(sentence, "\n"); 
 
-  *data = strlen(sentence);
-  int y = open("file.txt", O_WRONLY | O_APPEND, 0666);
-  write(y, sentence, *data);
-  close(y);
-  close(sentence);
+  *data = strlen(sentence); 
+  int y = open("file.txt", O_WRONLY | O_APPEND, 0666); 
+  write(y, sentence, *data); 
+  close(y); 
+  free(sentence); 
+  
 
-  us.val = 0;
-  semctl(semid, 0, SETVAL, us);
-  sb.sem_up = 1;
-  return 0;
-}
+  us.val = 0; 
+  semctl(semid, 0, SETVAL, us); 
+  return 0; 
+}  //end of main
+
+  
+/*   //closed or unused */
+/*   if (sb.sem_op == 1){ */
+      
+/*     printf("The game is currently being played by another user. Please wait your turn\n"); */
+
+/*     while( sb.sem_op == 1 );     */
+     
+/*   } */
+/*   //make a move */
+ 
+/*    //rest of code */
+
+/*   sb.sem_num = 0; */
+/*   sb.sem_flg = SEM_UNDO; */
+/*   sb.sem_op = -1; */
+/*   semop(semid,&sb,1); */
+ 
+/*   return 0; */
+
+
